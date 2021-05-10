5c832b354d820d8d1cd5476b4e1d4a4cd9c07935
BasavaRAJ <BasavaRAJ@DESKTOP-TNNOMRP>
2021-05-10 21:27:18 +0530

    push to local repo

---
+++ AutoDesk.Project/src/test/java/com/comcast/crm/organizationTest/CreateOrganizationTest.java
@@ -0,0 +1,16 @@
+package com.comcast.crm.organizationTest;
+
+import org.testng.annotations.Test;
+
+import com.crm.vtiger.GenericUtils.BaseClass1;
+import com.crm.vtiger.organizationpomclass.HomePage;
+
+public class CreateOrganizationTest extends BaseClass1{
+	@Test
+	public void createOrganizationTest()
+	{
+	//	String orgName=eUtil.getExcelData("org", 1, 2);
+		//homePage=new HomePage(driver);
+	}
+	
+}
